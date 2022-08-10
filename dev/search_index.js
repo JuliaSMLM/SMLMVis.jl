8459878323cf8ffdb68c1f46be8679c1260c8116
Documenter.jl <documenter@juliadocs.github.io>
2022-08-10 16:44:47 +0000

    build based on 3868bf7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SMLMVis","category":"page"},{"location":"#SMLMVis","page":"Home","title":"SMLMVis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SMLMVis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SMLMVis]","category":"page"}]
+}
