e5ccc829ba299e2661bd7c3d1df8668d191c8559
kalidke <kalidke@gmail.com>
2023-10-02 21:34:00 -0600

    doc updates

--- dev/simexample.jl
+++ dev/simexample.jl
@@ -4,6 +4,7 @@ using SMLMData
 using Images
 using SMLMVis
 
+# 2D
 smld_true, smld_model, smld_noisy = SMLMSim.sim(;
     ρ=10,
     σ_PSF=0.13,  
@@ -13,30 +14,27 @@ smld_true, smld_model, smld_noisy = SMLMSim.sim(;
     framerate=50.0, 
     pattern=SMLMSim.Nmer2D(),
     molecule=SMLMSim.GenericFluor(; q=[0 50; 1e-2 0]), #1/s 
-    camera=SMLMSim.IdealCamera(; xpixels=256, ypixels=256, pixelsize=0.1)
+    camera=SMLMSim.IdealCamera(; ypixels=32, xpixels=64, pixelsize=0.1)
     ) 
 
-smld_noisy.x *= 10.0
-smld_noisy.y *= 10.0
-smld_noisy.σ_x *= 10.0
-smld_noisy.σ_y *= 10.0
-smld_noisy.datasize *= 10
 out = render_blobs(smld_noisy; zoom = 20)
 save("nmer2d.png", out)
 
 # 3D
-# Simulation sequence
-n = length(smld_noisy.x)
-smld3d = SMLMData.SMLD3D(n)
-smld3d.x = smld_noisy.x
-smld3d.y = smld_noisy.y
-smld3d.z = rand(-1:.1:1, n)
-smld3d.σ_x = smld_noisy.σ_x
-smld3d.σ_y = smld_noisy.σ_y
-smld3d.datasize = smld_noisy.datasize
+smld_true, smld_model, smld_noisy = SMLMSim.sim(;
+    ρ=10,
+    σ_PSF=[0.13, 0.13, 0.3],  
+    minphotons=50,
+    ndatasets=10,
+    nframes=1000,
+    framerate=50.0, 
+    pattern=SMLMSim.Nmer3D(),
+    molecule=SMLMSim.GenericFluor(; q=[0 50; 1e-2 0]), #1/s 
+    camera=SMLMSim.IdealCamera(; ypixels=256, xpixels=128, pixelsize=0.1)
+    ) 
 
-length(smld3d.x)
-out = render_blobs(smld3d; zoom = 20)
+
+out = render_blobs(smld_noisy; zoom = 20)
 save("nmer3d.png", out)
 
 
--- docs/src/blob_rendering.md
+++ docs/src/blob_rendering.md
@@ -19,24 +19,54 @@ GaussRender
 
 2D SMLM images will render with the `hot` colormap by default.  
 
-```julia
+```@example
+using SMLMSim
 
+smld_true, smld_model, smld_noisy = SMLMSim.sim(;
+    ρ=10,
+    σ_PSF=0.13,  
+    minphotons=50,
+    ndatasets=10,
+    nframes=1000,
+    framerate=50.0, 
+    pattern=SMLMSim.Nmer2D(),
+    molecule=SMLMSim.GenericFluor(; q=[0 50; 1e-2 0]), #1/s 
+    camera=SMLMSim.IdealCamera(; ypixels=32, xpixels=64, pixelsize=0.1)
+    ) 
 
+out = render_blobs(smld_noisy; zoom = 20)
+save("nmer2d.png", out) # hide
+```
 
+```@raw html
+<img src="nmer2d.png" alt="nmer2d.png" width="600"/>
 ```
 
 ### Rendering 3D Images
 
 3D SMLM images will render with a rainbow colormap by default. 
-```julia
+```@example
+using SMLMSim
 
+smld_true, smld_model, smld_noisy = SMLMSim.sim(;
+    ρ=10,
+    σ_PSF=[0.13, 0.13, 0.3],  
+    minphotons=50,
+    ndatasets=10,
+    nframes=1000,
+    framerate=50.0, 
+    pattern=SMLMSim.Nmer3D(),
+    molecule=SMLMSim.GenericFluor(; q=[0 50; 1e-2 0]), #1/s 
+    camera=SMLMSim.IdealCamera(; ypixels=32, xpixels=64, pixelsize=0.1)
+    ) 
 
-
+out = render_blobs(smld_noisy; zoom = 20)
+save("nmer3d.png", out) # hide
 ```
 
-
-
-
+```@raw html
+<img src="nmer3d.png" alt="nmer3d.png" width="600"/>
+```
 
 ## API
 
