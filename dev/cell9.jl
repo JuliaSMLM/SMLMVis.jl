25f9d35c67de5d6f22fa01940cf443c869afabc8
Keith A. Lidke <kalidke@users.noreply.github.com>
2023-11-03 15:27:20 -0600

    localdata

---
+++ dev/cell9.jl
@@ -0,0 +1,15 @@
+using Revise 
+using HDF5
+using SMLMVis
+using ImageView
+
+# # TIRF Data 1 sequence
+dirname = "C:/Users/klidke/Downloads"
+filename = "Cell9_561laser5mW_exp0.05_300pM-2023-10-27-14-52-17"
+fn = joinpath(dirname, filename * ".h5")
+
+SMLMVis.MIC.count_datasets(fn)
+SMLMVis.MIC.isMIC(fn)
+data = Float32.(SMLMVis.MIC.readMIC(fn, datasetnum=1);)
+SMLMVis.MIC.mic2mp4(fn; crf = 10, fps = 20 )
+imshow(data)
