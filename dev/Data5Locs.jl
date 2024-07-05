e5f0aa890d853dfd1eaef4ab075c3a22cab5cc07
sajjad88 <56313813+sajjad88@users.noreply.github.com>
2024-07-05 16:09:11 -0600

    Added hist as a separate axis in same fig

--- dev/Data5Locs.jl
+++ dev/Data5Locs.jl
@@ -3,7 +3,7 @@ using SMLMVis
 using FileIO
 using SMLMData
 using Images
-using CairoMakie
+#using CairoMakie
 #using ImageView
 using GLMakie
 using Statistics
@@ -397,7 +397,7 @@ label_layout = GridLayout()
 fig[1, 1] = label_layout
 
 # Add a label to display the status at the top, centered in the figure
-status_label = Label(fig, text = "SR visualization", halign = :center, valign = :center)
+status_label = Label(fig, text = "SR image visualization", halign = :center, valign = :center)
 label_layout[1, 1] = status_label
 
 # Create a grid layout for the render button at the bottom
@@ -462,9 +462,6 @@ axislegend(ax2, position = :rt, legendfontsize = 50)
 println("Mean (μ): ", μ)
 println("Standard Deviation (σ): ", σ)
 display(fig2)
-
-
-
 # Extract x, y, and z coordinates for 3D scatter plot
 # x_coords = [point[2] for point in localizations]
 # y_coords = [point[1] for point in localizations]
@@ -474,6 +471,156 @@ display(fig2)
 # ax2 = CairoMakie.Axis3(fig2[1, 1], title = "Localizations 3D Scatter Plot", xlabel = "X", ylabel = "Y", zlabel = "Z")
 # scatter!(ax2, x_coords, y_coords, z_coords, markersize = 5, color = :blue) 
 # display(fig2)
+
+
+#=======================================================================================================#
+
+# Extract and Plot the smld.z within polygon and hist as separate fig
+z_range = (quantile(loc_data["z"], 0.01), quantile(loc_data["z"], 0.99))
+
+function get_polygon_points(p1, p2, p3, p4)
+    return [p1, p2, p3, p4, p1]
+end
+
+function interactive_plot_with_polygon_tool(ax, image)
+    points = Observable(Point2f0[])
+    mask_ready = Observable(false)
+    mask = Observable(Point2f0[])
+    
+    on(events(ax.scene).mousebutton) do event
+        if event.button == Mouse.left && event.action == Mouse.press
+            pos = mouseposition(ax.scene)
+            if all(pos .>= 0) && all(pos .<= Point2f0(size(image)))
+                push!(points[], pos)
+                notify(points)
+                scatter!(ax, [pos], color=:red, markersize=4)
+                if length(points[]) == 4
+                    polygon_points = get_polygon_points(points[][1], points[][2], points[][3], points[][4])
+                    lines!(ax, polygon_points, color=:red, linewidth=2)
+                    println("Coordinates of the polygon: ", polygon_points)
+                    mask[] = polygon_points
+                    println("The mask is: ", mask[])
+                    points[] = []
+                    notify(points)
+                    mask_ready[] = true
+                end
+            end
+        end
+    end
+    
+    return points, mask, mask_ready
+end
+
+function extract_polygon_data_and_points(smld, polygon_points, zoom)
+    println("Polygon points: ", polygon_points)
+
+    localizations = []
+    localizations_z = []
+
+    for i in 1:length(smld.x)
+        scaled_x = smld.x[i] * zoom
+        scaled_y = smld.y[i] * zoom
+        point = Point2f0(scaled_x, scaled_y)
+        if inpolygon(point, polygon_points) != 0
+            push!(localizations, (smld.y[i], smld.x[i]))
+            push!(localizations_z, smld.z[i])
+        end
+    end
+    
+    println("Number of localizations inside the polygon: ", length(localizations))
+    
+    return localizations, localizations_z
+end
+
+function render_image(button)
+    global smld, normalization, n_sigmas, colormap, z_range, zoom, percentile_cutoff, image_axis, localizations_z
+
+    if !isa(z_range, Tuple{Real, Real})
+        error("z_range is not of the correct type. It should be a tuple of two real numbers.")
+    end
+    
+    out, (cm, z_range) = SMLMVis.render_blobs(smld; normalization, n_sigmas, colormap, z_range, zoom, percentile_cutoff)
+    
+    image!(image_axis, out', show_axes = false)
+    status_label.text = "SR Image Rendered"
+
+    points, mask, mask_ready = interactive_plot_with_polygon_tool(image_axis, out)
+
+    on(mask_ready) do ready
+        if ready
+            localizations, localizations_z = extract_polygon_data_and_points(smld, mask[], zoom)
+            println("Number of localizations inside the polygon: ", length(localizations))
+        else
+            println("No polygon was defined.")
+        end
+    end
+end
+
+fig = Figure(resolution = (1000, 800))
+
+# Add the status label at the top
+status_label = Label(fig, text = "SR Image Visualization", halign = :center, valign = :center)
+
+# Define the axes and buttons
+global image_axis = GLMakie.Axis(fig[2, 1], title = "Render Image Axis", width = 500, height = 450)
+ax2 = GLMakie.Axis(fig[2, 2], title = "Histogram Axis", width = 400, height = 350)
+render_button = Button(fig[3, 1], label = "Render Image")
+hist_button = Button(fig[3, 2], label = "Plot Histogram")
+
+# Add a label to display the Gaussian fit parameters
+fit_params_label = Label(fig, text = "", halign = :left, valign = :top)
+
+# Arrange elements in the layout
+fig.layout.rowsizes = [Auto(0.1), Auto(1), Auto(0.1)]
+fig.layout.colsizes = [Auto(1), Auto(1)]
+
+# Add elements to the layout
+fig[1, 1:2] = status_label
+fig[2, 1] = image_axis
+fig[2, 2] = ax2
+fig[3, 1] = render_button
+fig[3, 2] = hist_button
+fig[4, 2] = fit_params_label
+
+function plot_histogram(button)
+    global localizations_z
+
+    # Clear the axis completely
+    empty!(ax2.scene)
+    
+    z_coords = Float64.(localizations_z)
+
+    hist!(ax2, localizations_z, bins = 70, color = 1, colormap = :tab10, colorrange = (1, 10), strokewidth = 0.4, strokecolor = :white)
+    fit_result = fit(Distributions.Normal, z_coords)
+    μ = fit_result.μ
+    σ = fit_result.σ
+
+    x = range(minimum(z_coords), stop = maximum(z_coords), length = 100)
+    y = pdf.(Distributions.Normal(μ, σ), x) * length(z_coords) * (maximum(z_coords) - minimum(z_coords)) / 70
+
+    label = "Gaussian fit: <d> = $(round(μ, digits=2)) nm, σ = $(round(σ, digits=2)) nm"
+    lines!(ax2, x, y, color = :red, linewidth = 4, label = label)
+
+    #axislegend(ax2, position = :rt, legendfontsize = 15)
+
+    # Update the fit parameters label
+    fit_params_label.text = label
+
+    println("Mean (μ): ", μ)
+    println("Standard Deviation (σ): ", σ)
+end
+
+on(render_button.clicks) do _
+    render_image(render_button)
+end
+
+on(hist_button.clicks) do _
+    plot_histogram(hist_button)
+end
+
+display(fig)
+
+
 
 
 
@@ -526,3 +673,142 @@ end
 
 file_path = "Y:/Projects/Super Critical Angle Localization Microscopy/Data/10-06-2023/Data5/smld.jld2"
 read_jld2_keys(file_path)
+
+
+
+#==============================================================================================================================#
+# In this aprt hist legend not gets updated:
+
+function get_polygon_points(p1, p2, p3, p4)
+    return [p1, p2, p3, p4, p1]
+end
+
+function interactive_plot_with_polygon_tool(ax, image)
+    points = Observable(Point2f0[])
+    mask_ready = Observable(false)
+    mask = Observable(Point2f0[])
+    
+    on(events(ax.scene).mousebutton) do event
+        if event.button == Mouse.left && event.action == Mouse.press
+            pos = mouseposition(ax.scene)
+            if all(pos .>= 0) && all(pos .<= Point2f0(size(image)))
+                push!(points[], pos)
+                notify(points)
+                scatter!(ax, [pos], color=:red, markersize=4)
+                if length(points[]) == 4
+                    polygon_points = get_polygon_points(points[][1], points[][2], points[][3], points[][4])
+                    lines!(ax, polygon_points, color=:red, linewidth=2)
+                    println("Coordinates of the polygon: ", polygon_points)
+                    mask[] = polygon_points
+                    println("The mask is: ", mask[])
+                    points[] = []
+                    notify(points)
+                    mask_ready[] = true
+                end
+            end
+        end
+    end
+    
+    return points, mask, mask_ready
+end
+
+function extract_polygon_data_and_points(smld, polygon_points, zoom)
+    println("Polygon points: ", polygon_points)
+
+    localizations = []
+    localizations_z = []
+
+    for i in 1:length(smld.x)
+        scaled_x = smld.x[i] * zoom
+        scaled_y = smld.y[i] * zoom
+        point = Point2f0(scaled_x, scaled_y)
+        if inpolygon(point, polygon_points) != 0
+            push!(localizations, (smld.y[i], smld.x[i]))
+            push!(localizations_z, smld.z[i])
+        end
+    end
+    
+    println("Number of localizations inside the polygon: ", length(localizations))
+    
+    return localizations, localizations_z
+end
+
+function render_image(button)
+    global smld, normalization, n_sigmas, colormap, z_range, zoom, percentile_cutoff, image_axis, localizations_z
+
+    if !isa(z_range, Tuple{Real, Real})
+        error("z_range is not of the correct type. It should be a tuple of two real numbers.")
+    end
+    
+    out, (cm, z_range) = SMLMVis.render_blobs(smld; normalization, n_sigmas, colormap, z_range, zoom, percentile_cutoff)
+    
+    image!(image_axis, out', show_axes = false)
+    status_label.text = "Image rendered"
+
+    points, mask, mask_ready = interactive_plot_with_polygon_tool(image_axis, out)
+
+    on(mask_ready) do ready
+        if ready
+            localizations, localizations_z = extract_polygon_data_and_points(smld, mask[], zoom)
+            println("Number of localizations inside the polygon: ", length(localizations))
+        else
+            println("No polygon was defined.")
+        end
+    end
+end
+
+fig = Figure(resolution = (1000, 800))
+
+# Add the status label at the top
+status_label = Label(fig, text = "SR image visualization", halign = :center, valign = :center)
+
+# Define the axes and buttons
+global image_axis = GLMakie.Axis(fig[2, 1], title = "Render Image Axis", width = 500, height = 450)
+ax2 = GLMakie.Axis(fig[2, 2], title = "Histogram Axis", width = 400, height = 350)
+render_button = Button(fig[3, 1], label = "Render Image")
+hist_button = Button(fig[3, 2], label = "Plot Histogram")
+
+# Arrange elements in the layout
+fig.layout.rowsizes = [Auto(0.1), Auto(1), Auto(0.1)]
+fig.layout.colsizes = [Auto(1), Auto(1)]
+
+# Add elements to the layout
+fig[1, 1:2] = status_label
+fig[2, 1] = image_axis
+fig[2, 2] = ax2
+fig[3, 1] = render_button
+fig[3, 2] = hist_button
+
+function plot_histogram(button)
+    global localizations_z
+
+    empty!(ax2.scene)
+    
+    z_coords = Float64.(localizations_z)
+
+    hist!(ax2, localizations_z, bins = 70, color = 1, colormap = :tab10, colorrange = (1, 10), strokewidth = 0.4, strokecolor = :white)
+    fit_result = fit(Distributions.Normal, z_coords)
+    μ = fit_result.μ
+    σ = fit_result.σ
+
+    x = range(minimum(z_coords), stop = maximum(z_coords), length = 100)
+    y = pdf.(Distributions.Normal(μ, σ), x) * length(z_coords) * (maximum(z_coords) - minimum(z_coords)) / 70
+
+    label = "Gaussian fit: <d> = $(round(μ, digits=2)) nm, σ = $(round(σ, digits=2)) nm"
+    lines!(ax2, x, y, color = :red, linewidth = 4, label = label)
+
+    axislegend(ax2, position = :rt, legendfontsize = 15)
+
+    println("Mean (μ): ", μ)
+    println("Standard Deviation (σ): ", σ)
+end
+
+on(render_button.clicks) do _
+    render_image(render_button)
+end
+
+on(hist_button.clicks) do _
+    plot_histogram(hist_button)
+end
+
+display(fig)
