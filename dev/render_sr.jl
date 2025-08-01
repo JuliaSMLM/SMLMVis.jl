6f78059b3b829c3417540e8555f507d731cc9620
kiwibogo <kiwibogo@gmail.com>
2025-08-01 15:48:47 -0600

    Add render_sr.jl for generating super-resolution images from SMLM data

---
+++ dev/render_sr.jl
@@ -0,0 +1,36 @@
+
+using Revise
+using HDF5
+using SMLMData
+using CairoMakie
+CM = CairoMakie
+using Statistics
+using ColorSchemes
+using Images
+
+datapath = raw"T:\projects\smart-microscope\data\DNA paint ruler\2025-07-15\python\result_1ch/"
+dataname = "20R-ruler-0.1exp-TIRF-onlyZFocusLockDT5-noPol--2025-07-15_01-32-51_loc_1ch_dme"
+filename = datapath * dataname * ".h5"
+f = h5open(filename, "r")
+x = read(f["res/x"])
+y = read(f["res/y"])
+σ_x = read(f["res/crlb"])[1, :]
+σ_y = read(f["res/crlb"])[2, :] 
+
+
+#smld = SMLMData.SMLD2D(0)
+x .-= minimum(x)
+y .-= minimum(y)
+datasize = ceil(maximum([maximum(x),maximum(y)]))
+datasize = [Int(datasize), Int(datasize)]
+srimg = Float32.(SMLMData.makegaussim([x y], [σ_x σ_y], datasize; mag = 20.0));
+
+
+srimg1 = deepcopy(srimg);
+Imax = quantile([srimg1...],0.999)
+Imax = Float32(Imax)
+srimg1[srimg1.>Imax].=Imax;
+srimg1 = float.(srimg1./ Imax);
+colored_img = map(x -> get(ColorSchemes.inferno, x), srimg1)
+save(datapath*dataname*"_SRimg.png",colored_img)
+
